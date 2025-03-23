e99228273c5b067198d014266137b80409e1d6ae
911turbodev <supragoodbye@gmail.com>
2025-03-22 21:52:13 -0300

    v.1

---
+++ calculadora.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{   
+    int opcion;
+    int num1, num2, num3, resultado;
+    printf("\nMenu calculadora\n");
+    printf("1. Suma\n");
+    printf("2. Resta\n");
+    printf("3. Multiplicacion\n");
+    printf("4. Division\n");
+    printf("6. Ordenar de mayor a menor tres valores\n");
+    printf("7. Salir\n");
+    printf("Elija una opcion para empezar\n");
+    scanf("%d", &opcion);
+
+
+    return 0;
+}
