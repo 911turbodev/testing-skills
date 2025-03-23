b077932bb05780ca7f40d7adf1f02086261b43a8
911turbodev <supragoodbye@gmail.com>
2025-03-23 17:05:58 -0300

    v1.0

--- Ejercicio-cajero.c
+++ Ejercicio-cajero.c
@@ -48,5 +48,5 @@ int main()
     return 0;
     //version finalizada del cajero
     //mas adelante agregare actualizaciones a medida que avance con mis conocimientos 03-22-2025
-    //version v.1
+    //version v.1.0
 }
