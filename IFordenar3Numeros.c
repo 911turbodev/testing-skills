62aa1ac273a8b534e586ef1cd5dc534cfc9e6fa7
911turbodev <supragoodbye@gmail.com>
2025-03-23 21:07:36 -0300

    v1.0

---
+++ IFordenar3Numeros.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Ingrese 3 numeros para ordenar de menor a mayor: \n");
+    scanf("%d %d %d", &a, &b, &c);
+    if (a > b)
+    return 0;
+}
