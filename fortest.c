3da3c2b0db7a0ce3b2d923216d651de8d35e68e2
911turbodev <supragoodbye@gmail.com>
2025-03-24 16:28:25 -0300

    v1.1

--- fortest.c
+++ fortest.c
@@ -2,7 +2,7 @@
 int main(){
     //prueba switch y for
     int opcion;
-    
+    do{    
     printf("Menu de seleccion\n");
     printf("1. Tabla de multiplicar\n");
     printf("2. Calculadora\n");
@@ -25,6 +25,39 @@ int main(){
             break;
         }
         case 2:{
+            int subopcion, n1, n2;
+            printf("Menu calculadora\n");
+            printf("1. Suma\n");
+            printf("2. Resta\n");
+            printf("3. Multiplicacion\n");
+            printf("4. Division\n");
+            printf("Elija una opcion por teclado: \n");
+            scanf("%d",&subopcion);
+            switch (subopcion){
+                case 1:
+                printf("Ingrese dos numeros para la suma: \n");
+                scanf("%d %d",&n1, &n2);
+                printf("\nResultado: %d\n",n1+n2);
+                break;
+                case 2:
+                printf("Ingrese dos numeros para la resta: \n");
+                scanf("%d %d",&n1, &n2);
+                printf("\nResultado: %d\n",n1-n2);
+                break;
+                case 3:
+                printf("Ingrese dos numeros para la multiplicacion: \n");
+                scanf("%d %d",&n1, &n2);
+                printf("\nResultado: %d\n",n1*n2);
+                break;      
+                case 4:
+                printf("Ingrese dos numeros para la division: \n");
+                scanf("%d %d",&n1, &n2);
+                printf("\nResultado: %d\n",n1/n2);
+                break;      
+            default:
+                printf("Valor invalido\n");
+                break;
+            }
            break; 
         }
         case 3:{
@@ -35,5 +68,6 @@ int main(){
         printf("Opcion no valida\n");
         break;
     }
+    }while (opcion != 3);    
     return 0;
 }
