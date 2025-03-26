2070752d4b4d34980aabf91e61c6ba69bb573896
911turbodev <supragoodbye@gmail.com>
2025-03-26 16:29:25 -0300

    v1.2

--- e5.c
+++ e5.c
@@ -16,10 +16,31 @@ int main()
     case 1: //suma
         printf("Ingrese dos numeros para sumar:\n");
         scanf("%d %d", &a, &b);
-        printf("Resultado: %d",a+b);
+        printf("Resultado: %d\n\n",a+b);
         break;
-    
+    case 2: //resta
+        printf("Ingrese dos numeros para restar:\n");
+        scanf("%d %d", &a, &b);
+        printf("Resultado: %d\n\n",a-b);\
+        break;
+    case 3: //multiplicacion
+        printf("Ingrese dos numeros para multiplicar:\n");
+        scanf("%d %d",&a, &b);
+        printf("Resultado: %d\n\n",a*b);
+        break;
+    case 4: //division
+        printf("Ingrese dos numeros para dividir:\n");
+        scanf("%d %d", &a, &b);
+        if (b > 0){
+            printf("Resultado: %d\n\n",a/b);
+        }else 
+            printf("El divisior no puede ser 0\n\n");
+        break;
+    case 5: //finalizar
+        printf("Finalizando el programa...\n");
+        break;
     default:
+        printf("Opcion invalida\n");
         break;
     }
     }while (opcion !=5);
