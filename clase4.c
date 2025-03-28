5dc667591adc8bc9c198ae8f49c40b665cda1980
911turbodev <supragoodbye@gmail.com>
2025-03-28 03:05:54 -0300

    v1.2

--- clase4.c
+++ clase4.c
@@ -13,21 +13,32 @@
 
 int main()
 {
-   int num, i=0, primo=1;
-   printf("Ingrese un numero para verificar si es primo (0 para salir): ");
-   scanf("%i", &num);
+   int num, i, primo;
    
-   do{
-           if (num > 0){
-               if (num % 2 == 0){
-                printf("%d: Es primo\n",num);
-            }else if ("num % 2 == 1"){
-                printf("%d: No es primo",num);
-                }else
-                    printf("gracias\n");
-            }     
-        }while (num != 0);
-    printf("\nFinalizando el programa...\n");
+   do {
+       printf("Ingrese un numero para verificar si es primo (0 para salir): ");
+       scanf("%i", &num);
+
+       if (num > 0) {
+           primo = 1; // Asumimos que es primo
+           for (i = 2; i <= num / 2; i++) {
+               if (num % i == 0) {
+                   primo = 0; // No es primo
+                   break;
+               }
+           }
+
+           if (primo) {
+               printf("%d: Es primo\n", num);
+           } else {
+               printf("%d: No es primo\n", num);
+           }
+       } else if (num < 0) {
+           printf("Por favor, ingrese un numero entero positivo.\n");
+       }
+   } while (num != 0);
+
+   printf("\nFinalizando el programa...\n");
 
 
 
